8df8ffd910263c222be3b2af5fb7ce68d4e6300f
zank <huazhankang@gmail.com>
2023-12-05 23:24:24 +0000

    complete

--- things/models.py
+++ things/models.py
@@ -8,9 +8,7 @@ from django.db import models
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.TextField(max_length=120, blank=True)
-    quantity = models.IntegerField(default=0, blank=False, validators=[
-        MinValueValidator(0),
-        MaxValueValidator(100)
-    ])
+    quantity = models.IntegerField()
+
 
 
